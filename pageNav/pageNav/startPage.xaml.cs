ae2aad7e86a9f80556648cf706a0a08e8f300d14
jb <belford.james@outook.com>
2016-03-07 21:57:14 -0800

    Done, just wanted to write this real quick so I have a place to start tomorrow

---
+++ pageNav/pageNav/startPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pageNav
+{
+    /// <summary>
+    /// Interaction logic for startPage.xaml
+    /// </summary>
+    public partial class startPage : Page
+    {
+        public startPage()
+        {
+            InitializeComponent();
+        }
+
+        private void signInBtn_Click(object sender, RoutedEventArgs e)
+        {
+            signIn s1 = new signIn();
+            this.NavigationService.Navigate(s1);
+        }
+
+        private void signOutBtn_Click(object sender, RoutedEventArgs e)
+        {
+            signOut s2 = new signOut();
+            this.NavigationService.Navigate(s2);
+        }
+    }
+}
